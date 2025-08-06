2888292aeb88ef2b403048b18549fec3f6bad463
hansrajathole <hansrajathole1@gmail.com>
2025-08-06 14:13:05 +0530

    Add initial HTML structure and JavaScript code

---
+++ LEC 1 JS/script.js
@@ -0,0 +1,29 @@
+// {
+//     let a = 10
+// }
+
+// console.log(a);
+
+
+// var a = 10
+// console.log(a);
+
+// var a = 20
+
+// console.log(a);
+
+
+// let a = 10
+// let a = 20
+
+// const b = 10
+
+
+let str = "raj"
+
+let copyStr = str
+
+console.log(str);
+
+copyStr = "Ayush"
+console.log(copyStr);
