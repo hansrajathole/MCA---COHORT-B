f0e6e85b921e417051dd52b632aced0525c1e03e
hansrajathole <hansrajathole1@gmail.com>
2025-08-07 11:52:13 +0530

    function and array methods

---
+++ LEC 2 JS/scipt.js
@@ -0,0 +1,90 @@
+// console.log(1 == "1");
+// console.log(1 === "1");
+
+
+// function declaration 
+// function fun() {
+//     console.log("raj");
+    
+// }
+
+// fun()
+
+
+// function expression 
+// let sum = function (a, b) {
+//     console.log(a+b); 
+// }
+
+
+// Arrow function 
+// let chacha = ()=>{
+//     console.log("red");
+    
+// }
+
+// chacha()
+
+
+// let a = 10
+// let str = "raj"
+// let istrue = true
+
+// console.log("a ===>", typeof a);
+// console.log("str ===>", typeof str);
+// console.log("istrue ===>", typeof istrue);
+// console.log("chacha ===>",typeof chacha);
+
+
+// Parameters vs Arguments
+
+
+// function fun(a, b) {
+//     console.log(a+b);
+    
+// }
+
+// fun(10,20)
+
+// function person( age = "99", name) {
+//     console.log(name);
+//     console.log(age);
+//     console.log(typeof age);
+ 
+// }
+
+// person()
+
+
+// function sum(a,b) {
+//     return a+b
+// }
+
+// console.log(sum(10,20));
+
+
+
+// Array 
+let arr = [10,20,30,40,50,60,70]
+// arr.push(50)
+// arr.pop()
+// arr.shift()
+
+
+// arr.unshift(5)
+
+// let slice = arr.slice(0, 4)
+// console.log(slice);
+// console.log(arr.slice(3,5));
+
+
+// arr.splice(2,3)
+// console.log(arr);
+
+
+// console.log(arr.includes(10));
+
+
+
+
+
