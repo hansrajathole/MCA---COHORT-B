df70a8e1ba270768e07ecddf5467bb2a75e87ba3
hansrajathole <hansrajathole1@gmail.com>
2025-08-12 12:00:48 +0530

    js advance

---
+++ LEC 4 JS/script.js
@@ -0,0 +1,83 @@
+
+
+
+// Math. floor
+
+// console.log(Math.floor(5/2));
+// console.log(Math.ceil(5/2));
+// console.log(Math.ceil(4));
+
+
+// console.log(Math.floor(Math.random()*10000));
+// console.log(Math.floor(Math.random()*6));
+// console.log(Math.floor(Math.random()*6));
+// console.log(Math.floor(Math.random()*6));
+
+
+
+// console.log(Math.max(10,20));
+// console.log(Math.min(10,20));
+
+// let naam = "raj"
+
+// console.log(`hello from ${4+4 > 5 ? "hansraj" : naam}`);
+// console.log("hello from ${4+4}");
+
+
+// let person = {
+//     name : "Abhishek",
+//     age : 21,
+//     mo : 987778670
+
+// }
+
+
+// for (const key in person) {
+   
+//     console.log(key , "===>" ,person[key]);
+    
+
+// }
+
+
+// let {name, ag} = person
+
+
+// console.log(name);
+// console.log(ag);
+
+
+// let arr = [10,20,30,40]
+
+
+// let copy = [...arr]
+
+// copy.push(50)
+// console.log(copy);
+// console.log(arr);
+
+
+
+// function sum(a, b, ...numbers) {
+//     let sum = 0
+//     for (const elem of numbers) {
+//         sum += elem
+        
+//     }
+    
+//     console.log(sum);
+    
+// }
+
+
+
+
+
+// sum(1,2,3,4,5,6,7,8)
+
+
+
+
+
+
+
