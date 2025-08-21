7d50c7497ded265498fc6303a61c38e25c0b1870
hansrajathole <hansrajathole1@gmail.com>
2025-08-21 11:49:36 +0530

    core modules

---
+++ LEC 11 NODE/app.js
@@ -0,0 +1,18 @@
+// const sum = require("./server")
+
+
+// sum(10,20)
+
+
+const fs = require("fs")
+
+
+// fs.writeFile("/text.txt","hello world",(err)=>{
+//     if(err){
+//         console.log(err);
+        
+//     }else {
+//         console.log("file was created");
+        
+//     }
+// })
---
+++ LEC 11 NODE/server.js
@@ -0,0 +1,14 @@
+// console.log("hello world");
+
+
+// function sum(a,b) {
+//     console.log(a+b);
+    
+// }
+
+
+// module.exports = sum
+
+
+
+
