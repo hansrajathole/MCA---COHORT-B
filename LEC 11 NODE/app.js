1139948f93cb772a45dc239cb96fe5ccf851fa87
hansrajathole <hansrajathole1@gmail.com>
2025-08-22 11:53:19 +0530

    fs all done

--- LEC 11 NODE/app.js
+++ LEC 11 NODE/app.js
@@ -6,51 +6,95 @@
 
 const fs = require("fs")
 
+// File
 
-fs.writeFileSync("text.txt","hello world",(err)=>{
-    if(err){
-        console.log(err);
+// fs.writeFileSync("text.txt","hello world",(err)=>{
+//     if(err){
+//         console.log(err);
         
-    }else {
-        console.log("file was created");
+//     }else {
+//         console.log("file was created");
         
-    }
-})
+//     }
+// })
 
 
 
-fs.readFileSync("text.txt", 'utf8',(err , data)=>{
-    if(err){
-        console.log(err);
+// fs.readFileSync("text.txt", 'utf8',(err , data)=>{
+//     if(err){
+//         console.log(err);
         
-    }else {
-        console.log("file data : ", data);
+//     }else {
+//         console.log("file data : ", data);
         
-    }
-})
+//     }
+// })
 
 
-fs.appendFileSync("text.txt", " hello guys kaise" , (err)=>{
-    if(err){
-        console.log(err);
+// fs.appendFileSync("text.txt", " hello guys kaise" , (err)=>{
+//     if(err){
+//         console.log(err);
         
-    }else {
-        console.log("file was updated");
+//     }else {
+//         console.log("file was updated");
 
-    }
-})
-
-
+//     }
+// })
 
 
-setTimeout(()=>{
-    fs.unlinkSync("text.txt",(err)=>{
-    if(err){
-        console.log(err);
+
+
+// setTimeout(()=>{
+//     fs.unlinkSync("text.txt",(err)=>{
+//     if(err){
+//         console.log(err);
         
-    }else {
-        console.log("file was delete");
+//     }else {
+//         console.log("file was delete");
         
-    }
-})
-},2000)
+//     }
+// })
+// },2000)
+
+
+
+
+
+// Folder
+
+
+// fs.mkdir("newFile",(err)=>{
+//     if(err){
+//         console.log(err);
+        
+//     }else {
+//         console.log("newfile created");
+//     }
+// })
+
+
+
+// fs.readdir("newFile",{recursive : true},(err , data)=>{
+//     if(err){
+//         console.log(err);
+        
+//     }else{
+//         console.log(data);
+        
+//     }
+
+// })
+
+
+// fs.rmdir("newFile", {recursive : true },(err)=>{
+//     if(err){
+//         console.log(err);
+        
+//     }else{
+//         console.log("chacha khatam ");
+        
+//     }
+// })
+
+
+
