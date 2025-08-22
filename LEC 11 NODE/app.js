28b606a09393915a7bf9154dd284a7f713ece4e3
hansrajathole <hansrajathole1@gmail.com>
2025-08-22 11:13:58 +0530

    file System , write , read , append , delete

--- LEC 11 NODE/app.js
+++ LEC 11 NODE/app.js
@@ -7,12 +7,50 @@
 const fs = require("fs")
 
 
-// fs.writeFile("/text.txt","hello world",(err)=>{
-//     if(err){
-//         console.log(err);
+fs.writeFileSync("text.txt","hello world",(err)=>{
+    if(err){
+        console.log(err);
         
-//     }else {
-//         console.log("file was created");
+    }else {
+        console.log("file was created");
         
-//     }
-// })
+    }
+})
+
+
+
+fs.readFileSync("text.txt", 'utf8',(err , data)=>{
+    if(err){
+        console.log(err);
+        
+    }else {
+        console.log("file data : ", data);
+        
+    }
+})
+
+
+fs.appendFileSync("text.txt", " hello guys kaise" , (err)=>{
+    if(err){
+        console.log(err);
+        
+    }else {
+        console.log("file was updated");
+
+    }
+})
+
+
+
+
+setTimeout(()=>{
+    fs.unlinkSync("text.txt",(err)=>{
+    if(err){
+        console.log(err);
+        
+    }else {
+        console.log("file was delete");
+        
+    }
+})
+},2000)
